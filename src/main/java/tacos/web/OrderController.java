489607c0fd58dfadacd08dbaa914b9163a39b231
Göktürk Gök <gokturkgok@gmail.com>
2021-03-08 03:39:45 +0300

    Spring Data JPA is implemented for relational databases.

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,34 +1,42 @@
 package tacos.web;
-
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 import javax.validation.Valid;
 
 @Controller
-@Slf4j
 @RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
 public class OrderController {
 
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
     @GetMapping("/current")
-    public String orderForm(Model model){
-        model.addAttribute("tacoOrder", new TacoOrder());
+    public String orderForm() {
         return "orderForm";
     }
 
     @PostMapping
-    public String processOrder(@Valid TacoOrder order, Errors errors) {
-        if(errors.hasErrors()){
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
             return "orderForm";
         }
 
-        log.info("Order submitted: " + order);
+        orderRepo.save(order);
+        sessionStatus.setComplete();
+
         return "redirect:/";
     }
+
 }
